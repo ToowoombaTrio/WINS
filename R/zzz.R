bb2b2c8604194a144b9426510bc6c3a4fbbf2857
Adam H. Sparks <adamhsparks@gmail.com>
2017-09-17 20:13:16 +1000

    Add startup message regarding data used in WINS

---
+++ R/zzz.R
@@ -0,0 +1,12 @@
+.onAttach <- function(libname, pkgname) {
+  msg <-
+    paste0(
+      "\nWINS uses data provided by the Australian Government Bureau of\n",
+      "Meteorology, which is released under the Creative Commons (CC)\n",
+      "Attribution 3.0 licence or Public Access Licence (PAL) as\n",
+      "appropriate, <http://www.bom.gov.au/other/copyright.shtml> and\n",
+      "hole-filled seamless SRTM data V4 aggregated to 250m available from\n",
+      "<http://srtm.csi.cgiar.org>.\n"
+    )
+  packageStartupMessage(msg)
+}
